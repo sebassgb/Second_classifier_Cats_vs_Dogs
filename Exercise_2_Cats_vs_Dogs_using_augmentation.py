857fc3dcca81c228473e65e4b72f34f7b06844d5
Sebastian Giraldo <sebassgb@users.noreply.github.com>
2020-06-13 22:54:17 +0200

    Create Exercise_2_Cats_vs_Dogs_using_augmentation.py

---
+++ Exercise_2_Cats_vs_Dogs_using_augmentation.py
@@ -0,0 +1,199 @@
+import os
+import zipfile
+import random
+import shutil
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile
+from os import getcwd
+
+path_cats_and_dogs = f"{getcwd()}/../tmp2/cats-and-dogs.zip"
+shutil.rmtree('/tmp')
+
+local_zip = path_cats_and_dogs
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+print(len(os.listdir('/tmp/PetImages/Cat/')))
+print(len(os.listdir('/tmp/PetImages/Dog/')))
+
+# Expected Output:
+# 1500
+# 1500
+
+try:
+    os.mkdir('/tmp/cats-v-dogs')
+    os.mkdir('/tmp/cats-v-dogs/training')
+    os.mkdir('/tmp/cats-v-dogs/testing')
+    os.mkdir("/tmp/cats-v-dogs/training/cats/");
+    os.mkdir("/tmp/cats-v-dogs/training/dogs/");
+    os.mkdir("/tmp/cats-v-dogs/testing/cats/");
+    os.mkdir("/tmp/cats-v-dogs/testing/dogs/");
+except OSError:
+    pass
+    
+ def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    
+    files_cd = os.listdir(SOURCE)
+    training = random.sample(files_cd, int(SPLIT_SIZE*len(files_cd))) 
+    train_set = set(training)
+    fnames_set = set(files_cd)
+    test_set  = fnames_set - train_set
+    train_set = list(train_set) 
+    test_set = list(test_set)
+
+    for train in train_set:
+        ftrain = SOURCE + train
+        if(os.path.getsize(ftrain) > 0):
+            destination = TRAINING + train
+            copyfile(ftrain, destination)
+    for test in test_set:
+        ftest = SOURCE + test
+        if(os.path.getsize(ftest) > 0):
+            destination = TESTING + test
+            copyfile(ftest, destination)
+
+
+CAT_SOURCE_DIR = "/tmp/PetImages/Cat/"
+TRAINING_CATS_DIR = "/tmp/cats-v-dogs/training/cats/"
+TESTING_CATS_DIR = "/tmp/cats-v-dogs/testing/cats/"
+DOG_SOURCE_DIR = "/tmp/PetImages/Dog/"
+TRAINING_DOGS_DIR = "/tmp/cats-v-dogs/training/dogs/"
+TESTING_DOGS_DIR = "/tmp/cats-v-dogs/testing/dogs/"
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+print(len(os.listdir('/tmp/cats-v-dogs/training/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/training/dogs/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/cats/')))
+print(len(os.listdir('/tmp/cats-v-dogs/testing/dogs/')))
+
+# Expected output:
+# 1350
+# 1350
+# 150
+# 150
+
+
+# DEFINE A KERAS MODEL TO CLASSIFY CATS V DOGS
+# USE AT LEAST 3 CONVOLUTION LAYERS
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fifth convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(), 
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'), 
+    # Only 1 output neuron. It will contain a value from 0-1 where 0 for 1 class ('cats') and 1 for the other ('dogs')
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
+
+TRAINING_DIR = "/tmp/cats-v-dogs/training/"
+train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# NOTE: YOU MUST USE A BATCH SIZE OF 10 (batch_size=10) FOR THE 
+# TRAIN GENERATOR.
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    batch_size=10,
+                                                    class_mode='binary',
+                                                    target_size=(300, 300))
+VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
+validation_datagen = ImageDataGenerator( rescale = 1.0/255. )
+
+# NOTE: YOU MUST USE A BACTH SIZE OF 10 (batch_size=10) FOR THE 
+# VALIDATION GENERATOR.
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                         batch_size=10,
+                                                         class_mode  = 'binary',
+                                                         target_size = (300, 300))
+
+
+# Expected Output:
+# Found 2700 images belonging to 2 classes.
+# Found 300 images belonging to 2 classes.
+
+history = model.fit_generator(train_generator,
+                              epochs=2,
+                              verbose=1,
+                              validation_data=validation_generator)
+                              
+ PLOT LOSS AND ACCURACY
+%matplotlib inline
+
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+
+
+plt.title('Training and validation loss')
+
+import numpy as np
+!pip install google-colab
+!pip install keras
+!pip install --upgrade pip
+from google.colab import files
+from keras.preprocessing import image
+
+uploaded=files.upload()
+
+for fn in uploaded.keys():
+ 
+  # predicting images
+  path='/content/' + fn
+  img=image.load_img(path, target_size=(300, 300))
+  
+  x=image.img_to_array(img)
+  x=np.expand_dims(x, axis=0)
+  images = np.vstack([x])
+  
+  classes = model.predict(images, batch_size=10)
+  
+  print(classes[0])
+  
+  if classes[0]>0:
+    print(fn + " is a dog")
+    
+  else:
+    print(fn + " is a cat")
